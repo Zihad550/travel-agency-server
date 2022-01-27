8da00f5fccc9d35739ec874b3eec42312dfa5e49
Zihad550 <hussainmuzahid83@gmail.com>
2022-01-27 17:39:13 +0600

    blog status updating functionality implemented

--- index.js
+++ index.js
@@ -4,15 +4,20 @@ const { MongoClient } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const ObjectId = require("mongodb").ObjectId;
+const fileUpload = require("express-fileupload");
 
 const port = process.env.PORT || 8000;
 
 // middle ware
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload());
 
 // mongo client
-const client = new MongoClient(process.env.URI);
+const client = new MongoClient(process.env.URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 async function run() {
   try {
@@ -23,8 +28,13 @@ async function run() {
 
     // get  blogs
     app.get("/blogs", async (req, res) => {
-      const { page, size } = req.query;
-      const cursor = blogsCollection.find({});
+      const { page, size, status } = req.query;
+      let cursor;
+      if (status === "approved") {
+        cursor = blogsCollection.find({ status });
+      } else {
+        cursor = blogsCollection.find({});
+      }
       let blogs;
       const count = await cursor.count();
       if (page) {
@@ -40,16 +50,29 @@ async function run() {
     });
 
     // save blogs
-    app.post("/blogs", async (req, res) => {
+    /* app.post("/blogs", async (req, res) => {
       const blog = req.body;
       const result = await blogsCollection.insertOne(blog);
       res.json(result);
-    });
+    }); */
 
     // delete a blog
     app.delete("/blogs", async (req, res) => {
       const id = req.query.id;
       const result = await blogsCollection.deleteOne({ _id: ObjectId(id) });
+      res.json(result);
+    });
+
+    app.post("/blogs", async (req, res) => {
+      const pic = req.files.src;
+      const picData = pic.data;
+      const encodedPic = picData.toString("base64");
+      const imageBuffer = Buffer.from(encodedPic, "base64");
+      const blog = {
+        ...req.body,
+        image: imageBuffer,
+      };
+      const result = await blogsCollection.insertOne(blog);
       res.json(result);
     });
   } finally {
